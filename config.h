8f6f1dcaf82820abd5d65dc8ce529394db1030c2
Igor Botukhov <igor.botukhov@gmail.com>
2025-07-27 21:00:15 +0200

    Added keyboard

--- config.h
+++ config.h
@@ -64,7 +64,8 @@ static const struct xkb_rule_names xkb_rules = {
 	/* example:
 	.options = "ctrl:nocaps",
 	*/
-	.options = NULL,
+    .options = "grp:caps_toggle",
+	.layout = "us(intl),ru",
 };
 
 static const int repeat_rate = 25;
